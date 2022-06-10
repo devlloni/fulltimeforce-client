d153bbdb4bec49dff3af84d0a02521912b2802d1
Lautaro DelloniÃ <dellonilautaro@gmail.com>
2022-06-10 16:53:31 -0300

    Added route map and index

---
+++ src/routes/index.routes.js
@@ -0,0 +1,13 @@
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+
+export default [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '*',
+        component: NotFound,
+    },
+];
